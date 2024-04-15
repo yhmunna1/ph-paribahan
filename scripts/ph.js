994319720f23b84f4007c30ef0c640cbd427baac
yhmunna1 <yh.munna.barisal@gmail.com>
2024-04-15 18:31:07 +0600

    select seat, add and remove from available, add seat details on the right side

---
+++ scripts/ph.js
@@ -0,0 +1,53 @@
+// Navigated Ticket Section
+function navigateToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+// Seat Section
+document.addEventListener("DOMContentLoaded", function () {
+  const seatButtons = document.querySelectorAll(".btn");
+  const seatAvailable = document.getElementById("seat-available");
+  const seatDetails = document.getElementById("seat-details");
+  const specificSeatDetails = document.getElementById("specific-seat-details");
+  console.log(specificSeatDetails);
+
+  let totalSeats = 0;
+  seatButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("bg-[#1DD100]");
+      this.classList.toggle("text-white");
+
+      const buttonNumber = this.id;
+
+      // Update total seat count
+      if (this.classList.contains("bg-[#1DD100]")) {
+        totalSeats++;
+        seatAvailable.innerText = parseInt(seatAvailable.innerText, 10) - 1;
+
+        // Add seat number on the right side
+        const seatDetailDiv = document.createElement("div");
+        seatDetailDiv.classList.add("flex", "justify-between", "py-2");
+        seatDetailDiv.innerHTML = `
+                    <div>${buttonNumber}</div>
+                    <div>Economy</div>
+                    <div>550</div>
+                `;
+        specificSeatDetails.appendChild(seatDetailDiv);
+      } else {
+        totalSeats--;
+        seatAvailable.innerText = parseInt(seatAvailable.innerText, 10) + 1;
+
+        // Remove seat number on the right side
+        const seatDetailDiv = specificSeatDetails.querySelectorAll("div");
+        seatDetailDiv.forEach((div) => {
+          if (div.textContent.trim() === buttonNumber) {
+            div.parentNode.remove();
+          }
+        });
+      }
+      console.log(`Selected button number: ${buttonNumber}`);
+      console.log(`Total selected seats: ${totalSeats}`);
+    });
+  });
+});
